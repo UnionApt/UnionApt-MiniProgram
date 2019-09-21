bbe021da908342f79dd3110f353b21fbef8265d5
Qinmin <wqm1997@gmail.com>
2019-09-20 23:43:46 -0700

    Prototype

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const mysql = require('mysql');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://root:root@localhost/";
+const img_dir = "http://192.168.0.21:8000/public/images/";
+var ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
@@ -15,15 +19,67 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('Hello World');
-})
+});
 
-var port = process.env.PORT || 8000
+app.use('/public', express.static('public'));
 
-var server = app.listen(port, function () {
+app.get('/getRoomListByRegion', function(req, res) {
+    var query_region = req.query.region;
 
-    var host = server.address().address
-    var port = server.address().port
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("aptInfo");
+        var whereStr = {"region":query_region};  // 查询条件
+        dbo.collection("aptInfo").find(whereStr).toArray(function(err, result) {
+            if (err) throw err;
+            result.forEach(function (entry) {
+                // entry.image = img_dir.concat(entry.image)
+                entry.image = img_dir + entry.image;
+                console.log(entry.image);
+            })
+            console.log(result);
+            res.status(200).send(result);
+            console.log(result);
+            res.end();
+            db.close();
+        });
+    });
+});
+
+app.get('/getRoomDetailsById', function(req, res) {
+    var query_id = req.query._id;
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("aptInfo");
+        var o_id = new ObjectId(query_id);
+        var whereStr = {"_id":o_id};  // 查询条件
+        dbo.collection("aptInfo").find(whereStr).toArray(function(err, result) {
+            if (err) throw err;
+            result.forEach(function (entry) {
+                // entry.image = img_dir.concat(entry.image)
+                entry.image = img_dir + entry.image;
+                console.log(entry.image);
+            });
+            console.log(result);
+            res.status(200).send(result);
+            console.log(result);
+            res.end();
+            db.close();
+        });
+    });
+});
+
+
+
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
 
     console.log("new instance launch at: http://%s:%s", host, port)
 
-})
+});
